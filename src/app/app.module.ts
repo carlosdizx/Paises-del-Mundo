a64cf7206e80783a9d1829e32afd2d475111a494
carlosdizx <carlodiaz@umariana.edu.co>
2021-05-31 23:30:46 -0500

    Creacion del header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaisesComponent } from './componentes/paises/paises.component';
+import { HeaderComponent } from './componentes/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaisesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
